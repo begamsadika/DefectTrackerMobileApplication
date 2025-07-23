cbe9bf4248f5120c8a183be8cf4ea08c265cface
begam <begamsadika817@gmail.com>
2025-07-23 20:07:38 +0530

    fix

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
 import Dashboard from './src/screens/Dashboard';
-import IconTest from './src/screens/IconTest';
+//import IconTest from './src/screens/IconTest';
 import ProjectDetails from './src/screens/ProjectDetails';
 
 const Stack = createNativeStackNavigator();
@@ -15,7 +15,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="Dashboard" component={Dashboard} />
-        <Stack.Screen name="IconTest" component={IconTest} options={{ title: 'Icon Test' }} />
+        {/* <Stack.Screen name="IconTest" component={IconTest} options={{ title: 'Icon Test' }} /> */}
         <Stack.Screen name="ProjectDetails" component={ProjectDetails} options={{ title: 'Project Details' }} />
       </Stack.Navigator>
     </NavigationContainer>
